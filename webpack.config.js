89a6854fc73e17954dee53039ea89ff1b918b91d
Latrop <32779759+Latrop1a@users.noreply.github.com>
2021-01-05 20:47:28 +0100

    front-end gerüst steht

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,30 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    filename: './js/bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: './bundle.js',
+    path: '/dist',
   },
   devServer: {
     contentBase: './dist',
+    watchContentBase: true,
     port: 3000,
     host: '127.0.0.1',
     open: true,
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html',
-    }),
-  ],
   mode: 'development',
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
       },
     ],
   },
