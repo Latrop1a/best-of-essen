2bfb626f0e3cf49984e7224669739fa8658f7dd4
Latrop <32779759+Latrop1a@users.noreply.github.com>
2021-02-23 14:37:34 +0100

    meal schema and some additions to mealController

--- server/data/controllers/mealController.js
+++ server/data/controllers/mealController.js
@@ -1,3 +1,5 @@
+import Meal from './../models/mealModel.js';
+
 const htmlRanklist = fs.readFileSync(`./src/templates/ranklist.html`, 'utf-8');
 
 const htmlRankBox = fs.readFileSync(`./src/templates/rank_box.html`, 'utf-8');
@@ -22,10 +24,50 @@ export const getRanklistPage = (req, res) => {
   res.status(500).json({ status: 'error', message: 'Not yet implemented.' });
 };
 
-export const getAllMealPage = (req, res) => {
-  res.status(500).json({ status: 'error', message: 'Not yet implemented.' });
+// gets all meals
+export const getAllMealPage = async (req, res) => {
+  try {
+    const meals = await Meal.find();
+    res.status(200).json({
+      status: 'success',
+      message: meals,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
-export const getMeal = (req, res) => {
-  res.status(500).json({ status: 'error', message: 'Not yet implemented.' });
+//gets a specific meal by id
+export const getMeal = async (req, res) => {
+  try {
+    const meal = await Meal.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      message: meal,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
+
+// gets a specific meal by rank
+export const getMealByRank = async (req, res) => {
+  try {
+    const meal = await Meal.findOne({ rank: req.params.id });
+    res.status(200).json({
+      status: 'success',
+      message: meal,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
--- server/data/models/mealModel.js
+++ server/data/models/mealModel.js
@@ -1,3 +1,47 @@
 import mongoose from 'mongoose';
 
-const Meal = 
+const mealSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Need a meal name'],
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: [true, 'Need a meal description'],
+  },
+  images: {
+    type: [String],
+    required: [true, 'Need a meal pic'],
+  },
+  ingredients: [String],
+  tags: [String],
+  effort: {
+    type: String,
+    required: [true, 'Need a meal effort'],
+  },
+  rank: {
+    type: Number,
+    default: 999,
+  },
+  elo: {
+    type: Number,
+    default: 1000,
+  },
+  battles: {
+    type: Number,
+    default: 0,
+  },
+  wins: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Meal = mongoose.model('Meal', mealSchema);
+
+export default Meal;
